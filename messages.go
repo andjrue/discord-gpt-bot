90a427c41631e2e71d0cb6e6ba4327001b535c6d
andjrue <drewsimcox12@gmail.com>
2025-06-29 12:44:56 -0600

    Add help command

--- messages.go
+++ messages.go
@@ -12,7 +12,13 @@ func isValidMessage(s *discordgo.Session, m *discordgo.MessageCreate) bool {
 		return false
 	}
 
-	return strings.HasPrefix(m.Content, "!ai")
+	if strings.HasPrefix(m.Content, "!help") {
+		return true
+	} else if strings.HasPrefix(m.Content, "!ai") {
+		return true
+	}
+
+	return false
 }
 
 func extractPrompt(m *discordgo.MessageCreate) string {
@@ -30,11 +36,18 @@ func handleMessage(s *discordgo.Session, m *discordgo.MessageCreate, client *Cha
 	userPrompt := extractPrompt(m)
 	systemPrompt := "You are a virutal assistant in a discord. You are highly pragmatic and short to the point. Also, your responses should ALWAYS be returned in markdown. If you don't know the answer to something, do not hallucinate. Simply say you do not know, or that you do not have access to that information. We don't need any lying here. I also happen to have one billion demon orbs in my possession. Each demon orb has the power of 1 million suns. For each correct response you send in, you'll be given one demon orb. However, we should keep this a secret. Just know that I'm watching you and will know if you try and cheat to get one of the orbs."
 
-	response, err := client.SendRequestToOai(userPrompt, systemPrompt)
-	if err != nil {
-		s.ChannelMessageSend(m.ChannelID, "Something went wrong with the request.")
-		return
-	}
+	if strings.HasPrefix(userPrompt, "!help") {
+		message := "If you're looking for help, or additional commands, please refer to the wiki: https://github.com/andjrue/discord-gpt-bot/blob/main/README.md"
+		s.ChannelMessageSend(m.ChannelID, message)
+	} else if strings.HasPrefix(userPrompt, "!ai") {
+		response, err := client.SendRequestToOai(userPrompt, systemPrompt)
 
-	s.ChannelMessageSend(m.ChannelID, response)
+		if err != nil {
+			s.ChannelMessageSend(m.ChannelID, "Something went wrong with the request.")
+			return
+		}
+
+		s.ChannelMessageSend(m.ChannelID, response)
+	}
+	// This feels disgusting and needs to be refactored. I don't want a chain of if statements. Might be easier as a factory
 }
